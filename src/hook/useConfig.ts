6243356ce595084ac1080c199d8974dbe8ae7b66
varuntree <varunprasad7989@gmail.com>
2024-04-26 22:43:17 +0530

    lasttest

--- src/hook/useConfig.ts
+++ src/hook/useConfig.ts
@@ -2,15 +2,18 @@ import { useConfigContext } from '../context/configContext'
 
 export const useConfig = (key: string, name: string) => {
   const { config } = useConfigContext()
-  return config?.[key]?.[name]
+  // Explicitly cast config to any
+  return (config as any)?.[key]?.[name]
 }
 
 export const useThemeConfig = (key: string) => {
   const { config } = useConfigContext()
-  return config?.[key]
+  // Explicitly cast config to any
+  return (config as any)?.[key]
 }
 
 export const useUiConfig = (key: string, name: string) => {
   const { uiConfig } = useConfigContext()
-  return uiConfig?.[key]?.[name]
+  // Explicitly cast uiConfig to any
+  return (uiConfig as any)?.[key]?.[name]
 }
