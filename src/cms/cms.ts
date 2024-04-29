286307612e0c8a0ed5d7e4c81e50f2c7e884d83c
varuntree <varunprasad7989@gmail.com>
2024-04-29 21:15:04 +0530

    m

--- src/cms/cms.ts
+++
@@ -1,8 +0,0 @@
-import CMS from 'decap-cms-app'
-import { NavbarPreview } from './preview-templates/NavbarPreview'
-
-
-
-CMS.registerPreviewTemplate('config', NavbarPreview)
-
-console.log("heloow this is indeed registerd")
---
+++ src/cmsSetup.tsx
@@ -0,0 +1,18 @@
+import { NavbarCms } from './cms_molecules/Navbar/index';
+import CMS from 'decap-cms-app';
+
+export function NavbarPreview({ entry }:{entry:any}) {
+    const data = entry.getIn(['data']).toJS();
+    return (
+        <div>
+            <NavbarCms 
+                size={data.size}
+                color={data.buttonConfig.color}
+                edge={data.buttonConfig.edge}
+            />
+        </div>
+    );
+}
+
+// Register the preview template with CMS
+CMS.registerPreviewTemplate('navbar', NavbarPreview);
